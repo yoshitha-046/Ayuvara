e1dfd21b1fb20aa259cfd908541cdc3b5877694a
yoshi086 <yvempata@student.gitam.edu>
2025-09-20 13:07:16 +0530

    Updated feature

--- src/pages/OptionPage.jsx
+++ src/pages/OptionPage.jsx
@@ -1,24 +1,26 @@
 import "./OptionPage.css";
 import Button from "../components/Button";
+
 export default function OptionPage() {
   return (
     <div className="background">
+
+      {/* Logo at the top */}
+      <img src="/Ayuvara.svg"   alt="Logo" className="logo"/>
+
         <div className="block">
             <div className="Op-block">
             {/* Patient Login Button */}
             <Button>Patient Login</Button>
 
             {/* Middle Text */}
-            <p className="text-[#B8860B] [font-family:'Arial','Helvetica',sans-serif]"> <span className="text-[2.5rem] [font-weight:700]"> 
-                Doctor ? 
-            </span> 
-            <span className="[font-family:'Poppins'] [font-weight:400] text-[2.25rem] [font-weight:600] text-center cursor-pointer" > 
-                Tap here
-            </span>
+            <p className="doctor-text">
+            <span className="doctor-title">Doctor ? </span>
+            <span className="doctor-tap">Tap here</span>
             </p>
 
             {/* Add space and SVG below */}
-            <img src="/arrow.svg" alt="Arrow" className="w-[50px] h-[50px] mt-10"  />
+            <img src="/arrow.svg" alt="Arrow" className="arrow"  />
 
             {/* Doctor Login Button */}
             <Button>Doctor Login</Button>
